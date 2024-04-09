a11a0144ffa0f70ea24e08744ed7171a0ad04a84
ilya <2002marsic@gmail.com>
2024-04-09 23:45:15 +0300

    integrations with ws

--- src/api/notes.ts
+++ src/api/notes.ts
@@ -1,5 +1,12 @@
 import axios from 'axios';
 import {Note} from '../types/notes';
+import {Repo} from '@automerge/automerge-repo';
+import {from} from '@automerge/automerge/next';
+
+function createAutomergeUrl(repo: Repo) {
+    const nextDoc = from({text: ''}); // TODO
+    return repo.create(nextDoc).url;
+}
 
 const api = axios.create({
     baseURL: `${import.meta.env.VITE_NOTES_URL}/api`,
@@ -13,8 +20,12 @@ export function listNotes() {
     return api.get<{notes: Note[]}>('/notes');
 }
 
-export function createNote(requestData: {title: string}) {
-    return api.post<Note>('/notes', requestData);
+export function createNote(requestData: {title: string}, repo: Repo) {
+    const note = {
+        title: requestData.title,
+        automerge_url: createAutomergeUrl(repo),
+    };
+    return api.post<Note>('/notes', note);
 }
 
 export function updateNote(requestData: Note) {
