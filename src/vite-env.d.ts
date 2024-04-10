f3dc0c85d2f5f0276656b3fdcea58a65df55df3d
ilya <2002marsic@gmail.com>
2024-04-10 04:13:14 +0300

    fixes on debounce

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,7 @@
 interface ImportMetaEnv {
     readonly VITE_YANDEX_API_KEY: string;
-    readonly VITE_PUBLIC_URL: string;
+    readonly VITE_ZOOM_URL: string;
+    readonly VITE_CHAT_URL: string;
     readonly VITE_NOTES_URL: string;
     readonly VITE_AUTOMERGE_URL: string;
 }
