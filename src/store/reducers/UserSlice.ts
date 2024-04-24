21032c06384a663ce8867bb4d8ca250b3cfb1377
ilya <2002marsic@gmail.com>
2024-04-25 01:28:30 +0300

    fixed

--- src/store/reducers/UserSlice.ts
+++ src/store/reducers/UserSlice.ts
@@ -1,20 +1,26 @@
 import {User} from '../../types/user';
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {loadUser, removeUser, saveUser} from '../../utils/localStorage';
 
 interface UserState {
     user: User | undefined;
 }
 
 const initialState: UserState = {
-    user: undefined,
+    user: loadUser(),
 };
 
 export const userSlice = createSlice({
     name: 'user',
     initialState: initialState,
     reducers: {
-        setUser(store, action: PayloadAction<User>) {
-            store.user = action.payload;
+        setUser(state, action: PayloadAction<User>) {
+            state.user = action.payload;
+            saveUser(action.payload);
+        },
+        logoutUser(state) {
+            state.user = undefined;
+            removeUser();
         },
     },
 });
