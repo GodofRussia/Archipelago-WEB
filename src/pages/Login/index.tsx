7aa0a53fac9789a766bd0bf56e3618d4cad91a66
ilya <2002marsic@gmail.com>
2024-04-24 19:46:31 +0300

    fixed

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -66,7 +66,7 @@ function Login() {
             dispatch(setUser(user));
             navigate('/');
         }
-    }, [dispatch, user]);
+    }, [dispatch, navigate, user]);
 
     return (
         <Box
