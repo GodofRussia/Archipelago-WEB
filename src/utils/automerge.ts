945148b99f8fbf0b4909def9682c53111e90a02b
ilya <2002marsic@gmail.com>
2024-04-25 02:44:23 +0300

    fixed

--- src/utils/automerge.ts
+++ src/utils/automerge.ts
@@ -1,10 +1,9 @@
 import {Repo} from '@automerge/automerge-repo';
-import {from} from '@automerge/automerge/dist/next';
 import * as A from '@automerge/automerge';
 import {NoteDoc} from '../types/notes';
 
 export function createAutomergeUrl(repo: Repo) {
-    const nextDoc = from({text: new A.Text()});
+    const nextDoc = A.from({text: new A.Text()});
     const handle = repo.create<NoteDoc>(nextDoc);
 
     return handle.url;
