f99c042de7626ad36456977de1705b46b3888788
ilya <2002marsic@gmail.com>
2024-04-25 03:52:13 +0300

    fixed

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -49,6 +49,7 @@ const NoteCard: React.FC<NoteCardProps> = ({
         cursor: 'pointer',
     };
 
+    console.log(activeNote, activeStyle);
     const handleDeleteClick = React.useCallback(
         async (ev: React.MouseEvent, note: Note) => {
             ev.stopPropagation();
